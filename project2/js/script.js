6280f5d30e0ad530c527184eb36d9ba17fcc6319
sal <sbennekaa@gmail.com>
2017-07-24 15:47:33 +0000

    first

---
+++ project2/js/script.js
@@ -0,0 +1,22 @@
+/*global $ */
+
+/*$(document).ready(function() {
+    $("#toggle").click(function(){
+        $("#toggle").toggle("fade", 6000).fadeIn();
+        
+    });
+    
+}); */
+
+/*$( document ).click(function() {
+  $( "#toggle" ).toggle("explode", { pieces: 16 }, 2000);
+});*/
+
+
+/*$(document).ready(function() {
+    $("#toggle").click(function(){
+        $("#toggle").explode(2000);
+      
+    });
+    
+});*/
